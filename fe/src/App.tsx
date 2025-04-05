ec4693f7d74735a54643d37a477b007a20845552
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-04-05 15:17:07 +0700

    feat: add layout for main section

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,9 +1,38 @@
+import { Box, Typography } from "@mui/joy";
 import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <>
-      <SearchBar onSearch={() => {}} initialValue="Input text"></SearchBar>
+      <Box
+        sx={{
+          width: "auto",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 2,
+          padding: 2,
+        }}
+      >
+        <Typography level="h1" sx={{ textAlign: "center" }}>
+          Tracking Assistant
+        </Typography>
+        <Box
+          sx={{
+            width: "80vw",
+            height: "80vh",
+            border: "1px solid black",
+            borderRadius: "lg",
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            padding: 1,
+            alignItems: "center",
+          }}
+        >
+          <SearchBar onSearch={() => {}} initialValue=""></SearchBar>
+        </Box>
+      </Box>
     </>
   );
 }
