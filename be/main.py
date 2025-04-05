d775427f8343b1ac8e73188487462ec47a4812b6
devkcng <kimmanhcuong230304@gmail.com>
2025-04-05 21:14:26 +0700

    feat: add upload video route

--- be/main.py
+++ be/main.py
@@ -24,3 +24,13 @@ def read_root():
 def search(request: SearchRequest):
     result = faiss_engine.search(request.text, k=request.k)
     return result
+
+
+@app.post("/upload-video/")
+async def upload_video(file: UploadFile = File(...)):
+    save_path = "static/videos/unprocessed"
+    os.makedirs(save_path, exist_ok=True)
+    file_location = os.path.join(save_path, file.filename)
+    with open(file_location, "wb") as f:
+        f.write(await file.read())
+    return {"message": f"Video {file.filename} uploaded successfully!"}
